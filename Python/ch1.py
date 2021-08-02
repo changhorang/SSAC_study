8648da0ca126759adf5ee91090df46af6eb6e29d
changhorang <64844315+changhorang@users.noreply.github.com>
2021-08-02 22:34:08 +0900

    update

---
+++ Python/ch1.py
@@ -0,0 +1,19 @@
+# 자료형과 문자열
+# 1. 문자열 (string) : "abcd"...
+# 2. 숫자 (number) : 1, 2, 3,...
+# 3. 불 (boolean) : True, False
+
+print(type(273.0)) # float
+print(type(273)) # int
+print(type("hello")) # str
+
+# "" 넣기 : ''&"" 동시 사용 또는 아래와 같이 사용
+print("\"h\"ello") # "h"ello
+
+# \n : 줄바꿈
+# \t : tab 추가
+# "" + "" : 문자열 연결 (같은 타입끼리)
+# "" * int : 문자열 반복
+
+# [] 인덱싱 / [:] 슬라이싱
+# len() : 문자열의 길이 반환
