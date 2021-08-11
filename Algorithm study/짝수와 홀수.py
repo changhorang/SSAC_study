a4728d48de880d3e3931b78100921d8929322d50
changhorang <64844315+changhorang@users.noreply.github.com>
2021-08-12 02:27:37 +0900

    add

---
+++ Algorithm study/짝수와 홀수.py
@@ -0,0 +1,11 @@
+def solution(num):
+    answer = ''
+    if num % 2 == 0:
+        answer = "Even"
+    else:
+        answer = "Odd"
+    return answer
+
+#아래는 테스트로 출력해 보기 위한 코드입니다.
+print("결과 : " + solution(3))
+print("결과 : " + solution(2))
