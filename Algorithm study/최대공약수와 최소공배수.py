f1a384e47a48c399b01b518eda8924913bc5897d
changhorang <64844315+changhorang@users.noreply.github.com>
2021-08-18 17:58:35 +0900

    commit

---
+++ Algorithm study/최대공약수와 최소공배수.py
@@ -0,0 +1,12 @@
+def solution(a, b):
+    c, d = max(a, b), min(a, b)
+    t = 1
+    while t > 0:
+        t = c % d
+        c, d = d, t
+    answer = [c, int(a*b/c)]
+
+    return answer
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(solution(3,12))
