6292aaac7998899bd76012a7cde36a3fb5d8de28
changhorang <64844315+changhorang@users.noreply.github.com>
2021-08-22 18:58:35 +0900

    Create 시저 암호.py

---
+++ Algorithm study/시저 암호.py
@@ -0,0 +1,20 @@
+def solution(s, n):
+    lower_list = "abcdefghijklmnopqrstuvwxyz"
+    upper_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    result = []
+
+    for i in s:
+        if i is " ":
+            result.append(" ")
+        elif i.islower() is True:
+            new_ = lower_list.find(i) + n
+            result.append(lower_list[new_ % 26])
+        else:
+            new_ = upper_list.find(i) + n
+            result.append(upper_list[new_ % 26])
+    return "".join(result)
+
+
+# 실행을 위한 테스트코드입니다.
+print('s는 "a B z", n은 4인 경우: ' + caesar("a B z", 4))
